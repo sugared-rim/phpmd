411ed973fc1cfc5302f2fa083a6494a0c1c89079
Michael Mayer <michael@schnittstabil.de>
2016-09-26 19:59:54 +0200

    bump dependencies

--- src/TextUI/CommandLineOptions.php
+++ src/TextUI/CommandLineOptions.php
@@ -6,8 +6,23 @@ class CommandLineOptions extends \PHPMD\TextUI\CommandLineOptions
 {
     public function __construct(array $args, array $availableRuleSets = array(), $defaults = null)
     {
+        $this->parseDefaults($args, $defaults);
+
+        try {
+            parent::__construct($args, $availableRuleSets);
+        } catch (\InvalidArgumentException $err) {
+            if ($err->getCode() !== self::INPUT_ERROR) {
+                throw $err;
+            }
+        }
+
+        $this->assertValidState();
+    }
+
+    protected function parseDefaults(&$args, $defaults = null)
+    {
         if ($defaults === null) {
-            $defaults = new \stdClass();
+            return;
         }
 
         foreach (get_object_vars($defaults) as $k => $v) {
@@ -22,15 +37,10 @@ class CommandLineOptions extends \PHPMD\TextUI\CommandLineOptions
                     $this->$k = $v;
             }
         }
+    }
 
-        try {
-            parent::__construct($args, $availableRuleSets);
-        } catch (\InvalidArgumentException $err) {
-            if ($err->getCode() !== self::INPUT_ERROR) {
-                throw $err;
-            }
-        }
-
+    protected function assertValidState()
+    {
         if (empty($this->inputPath) || empty($this->reportFormat) || empty($this->ruleSets)) {
             throw new \InvalidArgumentException($this->usage(), self::INPUT_ERROR);
         }
